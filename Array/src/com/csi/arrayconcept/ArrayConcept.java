2f75d5abedcea346949c175c9bb91edd8dbde645
Ganeshkasar123 <gkasar39@gmail.com>
2021-11-03 10:10:34 +0530

    Changes in array

--- Array/src/com/csi/arrayconcept/ArrayConcept.java
+++ Array/src/com/csi/arrayconcept/ArrayConcept.java
@@ -4,6 +4,9 @@ public class ArrayConcept {
 	public static void main(String[] args) {
 		int arr[]={2,3,4,5,5, };
 		System.out.println(arr.length);
+
+		int array[]={4,8,6,5,6,6,};
+		System.out.println(array.length);
 	}
 
 }
